5fd8be16b6856745e555a6fc0e4e077d9835cf65
BobDaDino <w6zhu@ucsd.edu>
2023-11-07 09:14:02 -0800

    update grade

--- grade.sh
+++ grade.sh
@@ -14,7 +14,10 @@ cd student-submission
 if [[ -f ListExamples.java ]]
 then
     cp -r ListExamples.java ../grading-area
-    echo 'test'
+    cd ..
+    javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples.java
+
 else
     echo 'resubmit with proper file'
 fi
